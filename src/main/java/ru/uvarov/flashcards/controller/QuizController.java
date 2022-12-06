9691b5ea9002bd9afed44a31e4061ece737edc91
Aleksandr Uvarov <aleksandr.o.uvarov@raiffeisen.ru>
2022-12-06 22:42:57 +0100

    Use list in view

--- src/main/java/ru/uvarov/flashcards/controller/QuizController.java
+++ src/main/java/ru/uvarov/flashcards/controller/QuizController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.uvarov.flashcards.model.Question;
 import ru.uvarov.flashcards.service.QuizService;
 
 @Controller
@@ -15,12 +14,7 @@ public class QuizController {
 
     @GetMapping("/")
     public String quiz(Model model) {
-        Question question = quizService.getQuestion();
-        model.addAttribute("word", question.getWord());
-        model.addAttribute("answer1", question.getAnswersList().get(0).getWord());
-        model.addAttribute("answer2", question.getAnswersList().get(1).getWord());
-        model.addAttribute("answer3", question.getAnswersList().get(2).getWord());
-        model.addAttribute("answer4", question.getAnswersList().get(3).getWord());
+        model.addAttribute("question", quizService.getQuestion());
         return "quiz";
     }
 }
