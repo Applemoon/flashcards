77a0f59127e31bb5ef5c31ddae4ccc3f6690b1b1
Aleksandr Uvarov <aleksandr.o.uvarov@raiffeisen.ru>
2022-12-07 11:03:42 +0100

    Much more user friendly

--- src/main/java/ru/uvarov/flashcards/model/Answer.java
+++ src/main/java/ru/uvarov/flashcards/model/Answer.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Answer {
     private String word;
+    private String translate;
     private Boolean isRight;
 
-    public Answer(String word) {
+    public Answer(String word, String translate) {
         this.word = word;
+        this.translate = translate;
         this.isRight = false;
     }
 }
