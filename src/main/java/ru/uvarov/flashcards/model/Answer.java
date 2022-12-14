398bded80bc2a71752d2f4c0ed35941c3ab8fe1a
Aleksandr Uvarov <aleksandr.o.uvarov@raiffeisen.ru>
2022-12-15 00:11:07 +0100

    Fix answers comparing

--- src/main/java/ru/uvarov/flashcards/model/Answer.java
+++ src/main/java/ru/uvarov/flashcards/model/Answer.java
@@ -1,9 +1,23 @@
 package ru.uvarov.flashcards.model;
 
 
+import java.util.Objects;
+
 public record Answer(
     String word,
     String translate,
     boolean isRight
 ) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answer answer = (Answer) o;
+        return word.equals(answer.word) && translate.equals(answer.translate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, translate);
+    }
 }
