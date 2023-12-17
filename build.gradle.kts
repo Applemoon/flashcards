0ff414a5d7c896af9e6ca1f455c9cfea5365cae2
Aleksandr Uvarov <sanyauvarov@mail.ru>
2023-12-17 09:23:28 +0100

    Java 17 -> 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.0.0"
-	id("io.spring.dependency-management") version "1.1.0"
+	id("org.springframework.boot") version "3.2.0"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "ru.uvarov"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+
+java {
+	targetCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_21
+}
 
 configurations {
 	compileOnly {
--- src/main/java/ru/uvarov/flashcards/service/FileService.java
+++ src/main/java/ru/uvarov/flashcards/service/FileService.java
@@ -1,9 +1,5 @@
 package ru.uvarov.flashcards.service;
 
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,21 +7,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Getter
 @Service
 public class FileService {
 
-    @Getter
     private final List<String> fileContent;
 
-    @Getter
     private final Map<String, String> wordPairs;
 
     public FileService(@Value("${quiz.filename}") String fileName) {
         InputStream inputStream = Objects.requireNonNull(getClass().getResourceAsStream(fileName));
-        fileContent = new BufferedReader(new InputStreamReader(inputStream))
-            .lines()
-            .collect(Collectors.toList());
+        fileContent = new BufferedReader(new InputStreamReader(inputStream)).lines().toList();
 
         wordPairs = fileContent.stream()
             .filter(line -> !line.startsWith("#"))
@@ -33,9 +29,10 @@ public class FileService {
             .peek(line -> {
                 if (!line.contains("=")) throw new IllegalArgumentException(line);
             })
-            .map(line -> line.trim().split("="))
+            .map(String::trim)
+            .map(line -> line.split("="))
             .collect(Collectors.toMap(it -> it[0].trim(), it -> it[1].trim()));
 
-        System.out.println("Найдено " + wordPairs.size() + " слов");
+        System.out.printf("Найдено %s слов%n", wordPairs.size());
     }
 }
