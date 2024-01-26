f6408badb4c979e2e0ebe408d7f86f30e5ef52c3
Aleksandr Uvarov <sanyauvarov@mailru>
2024-01-26 22:55:53 +0100

    Save quiz errors

--- src/main/java/ru/uvarov/flashcards/FlashcardsApplication.java
+++ src/main/java/ru/uvarov/flashcards/FlashcardsApplication.java
@@ -1,11 +1,20 @@
 package ru.uvarov.flashcards;
 
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FlashcardsApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FlashcardsApplication.class, args);
+        try {
+            new ProcessBuilder("open", "http://localhost:8080").start();
+        } catch (IOException e) {
+            log.error("Failed to run browser", e);
+        }
     }
 }
